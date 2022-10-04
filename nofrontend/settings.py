b5ab397634e5e947d39f04e1b0ce08a1f8993808
skinod123 <spike1990amir@gmail.com>
2022-10-04 18:44:23 +0600

    added db

--- nofrontend/settings.py
+++ nofrontend/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'nofrontend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': env("POSTGRES_DB"),
         'USER': env("POSTGRES_USER"),
         'PASSWORD': env("POSTGRES_PASSWORD"),
         'HOST': env("POSTGRES_HOST"),
