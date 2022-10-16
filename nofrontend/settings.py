7eec424092ce2602e172c118509ff5fdfe6da09a
skinod123 <spike1990amir@gmail.com>
2022-10-16 17:41:00 +0600

    lower flower

--- nofrontend/settings.py
+++ nofrontend/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'nofrontend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env("POSTGRES_DB"),
         'USER': env("POSTGRES_USER"),
         'PASSWORD': env("POSTGRES_PASSWORD"),
